cac291630a551d29318367ff8316dfa60cd322c4
Shea Stewart <stewart.shea@gmail.com>
2024-06-13 16:29:14 +0000

    clean up created container

--- codebundles/k8s-namespace-healthcheck/workload_issues.sh
+++ codebundles/k8s-namespace-healthcheck/workload_issues.sh
@@ -100,7 +100,7 @@ if [[ $messages =~ "OCI runtime exec failed: exec failed: unable to start contai
     add_issue "2" "Possible node or container runtime issue" "$messages" "Escalate container runtime issue to service owner if they continue."
 fi
 
-if [[ $messages =~ "Created container server" || $messages =~ "no changes since last reconcilation" || $messages =~ "Reconciliation finished" ]]; then
+if [[ $messages =~ "Created container" || $messages =~ "no changes since last reconcilation" || $messages =~ "Reconciliation finished" ]]; then
     # Don't generate any issue data, these are normal strings
     echo "[]" | jq .
     exit 0
